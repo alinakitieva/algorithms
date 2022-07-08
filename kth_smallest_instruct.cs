4c874043af6830c781a39bd9def2fad95abcb164
Alina <96070394+alinakitieva@users.noreply.github.com>
2022-07-08 11:23:57 +0300

    add comments

--- kth_smallest_instruct.cs
+++ kth_smallest_instruct.cs
@@ -7,6 +7,9 @@ public class Solution
 
         int[,] dp = new int[rows+1, columns+1];
 
+
+        // Here we are filling up the dp in such a way that each element
+        // is the number of ways to reach the destination
         for (int row=rows; row>-1; row--)
         {
             for (int column=columns; column>-1; column--)
@@ -29,19 +32,20 @@ public class Solution
         {
             if (v == rows && h == columns)
                 return answer;
-            if (v == rows)
+            if (v == rows)  // all V's are used so we just need to add H's
             {
                 answer += "H";
                 h += 1;
             }
-            else if (h == columns)
+            else if (h == columns) // all H's are used so we just need to add V's
             {
                 answer += "V";
                 v += 1;
             }
             else
             {
-                int right = dp[v, h + 1];
+                int right = dp[v, h + 1]; // right is the number of ways to reach the destination
+                                          // if we move horizontally
                 if (k > right)
                 {
                     k -= right;
